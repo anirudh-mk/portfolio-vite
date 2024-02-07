b325b033ca06904108b45255b1bba784de099cb7
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-07 23:33:32 +0530

    feat(app) Project and Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <>
       <Home />
       <About />
+      <Projects />
+      <Contact />
     </>
   );
 }
