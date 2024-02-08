1f2f41f19533aed9a21c7e6c941c06a0a46ddf09
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-08 18:10:22 +0530

    feat(Projects)

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Projects() {
-  return <div className="py-[20px]"></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/anirudh-mk/repos", {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_GITHUB}`,
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {});
+  });
+  return (
+    <div className="py-[20px]">
+      <h1 className="text-[22px] font-bold pl-[20px]">Projects</h1>
+      <ul>
+        {data.map((project) => (
+          <p key={project.id}>{project.name}</p>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Projects;
