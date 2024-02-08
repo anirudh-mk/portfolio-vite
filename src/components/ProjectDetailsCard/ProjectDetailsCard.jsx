1a8b1724a857d2871e3ee832d465a0d8c27b12f8
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-08 23:23:41 +0530

    feat(ProjectDetailsCard)

--- src/components/ProjectDetailsCard/ProjectDetailsCard.jsx
+++ src/components/ProjectDetailsCard/ProjectDetailsCard.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import "./ProjectDetailsCard.css";
 
 function ProjectDetailsCard({ data }) {
   return (
-    <div className="my-[20px] mx-[40px] bg-[#6E6F64] p-[20px] rounded-xl">
+    <div className="my-[20px] mx-[40px] bg-[#6E6F64] p-[20px] rounded-xl project-details-card">
       <a href={data.html_url}>
-        <h1 className="text-white text-[20px] font-semibold">
+        <h1 className="text-white text-[20px] font-semibold overflow-text">
           {data.name
             .replace(/[_-]/g, " ")
             .replace(/\b\w/g, (c) => c.toUpperCase())}
         </h1>
-        <p className="text-white text-[18px]">
+        <p className="text-white text-[18px] py-[10px] overflow-text">
           {data.description == null
             ? data.name
                 .replace(/[_-]/g, " ")
                 .replace(/\b\w/g, (c) => c.toUpperCase())
             : data.description}
         </p>
-        <p className="text-white text-[18px] text-right">
-          <i>{data.language}</i>
+        <p className="text-white text-[18px] pt-[10px] text-right">
+          <i>{data.language == null ? "None" : data.language}</i>
         </p>
       </a>
     </div>
