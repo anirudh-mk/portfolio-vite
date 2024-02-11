aca74f2aa404494f0b5f69c34abb01b16822fc1a
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-11 21:23:25 +0530

    feat(Home):Mediam Screen

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,8 +13,8 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   return (
-    <div className="home grid grid-row-2 gap-[30px] pb-[80px]">
-      <div className="flex justify-center pt-[30px]">
+    <div className="home grid grid-row-2 gap-[30px] pb-[80px] pt-[100px] md:grid-cols-2 md:gap-[20px] md:px-[80px] lg:w-[1192px] lg:mx-auto">
+      <div className="flex justify-center pt-[30px] md:hidden">
         <ProfileImage
           image={AnirudhImage}
           width={300}
@@ -23,13 +23,13 @@ function Home() {
         />
       </div>
       <div className="flex justify-center grid grid-row-2 gap-[30px]">
-        <h1 className="text-[30px] leading-[3.5rem] font-bold">
+        <h1 className="text-[30px] leading-[3.5rem] font-bold md:px-[30px] md:text-[40px] md:leading-[5rem]">
           Hi <br />
           I'am Anirudh MK
           <br />
           Web Developer
         </h1>
-        <div className="grid grid-row-2 gap-[30px]">
+        <div className="grid grid-row-2 gap-[30px] md:grid-cols-2 md:gap-0 md:justify-end">
           <div className="flex justify-center">
             <TextButton
               button_text="Download CV"
@@ -37,7 +37,7 @@ function Home() {
               text_button_color="#6E6F64"
             />
           </div>
-          <div className="flex justify-center grid-cols-4 gap-[20px]">
+          <div className="flex justify-center grid-cols-4 gap-[20px] md:justify-start">
             <IconButton
               icon={faGithubAlt}
               link="https://github.com/anirudh-mk"
@@ -56,6 +56,14 @@ function Home() {
             <IconButton icon={faTwitter} icon_button_color="#6E6F64" />
           </div>
         </div>
+      </div>
+      <div className="hidden justify-center pt-[30px] md:flex">
+        <ProfileImage
+          image={AnirudhImage}
+          width={344}
+          height={403}
+          border_radius={172}
+        />
       </div>
     </div>
   );
